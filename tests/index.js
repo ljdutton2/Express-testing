112ea94c53a99b41c97198912ad85026bcabdd05
lauren <ljdutton2@icloud.com>
2020-07-01 10:19:30 -0700

    init

---
+++ tests/index.js
@@ -0,0 +1,37 @@
+/* src/test/index.js */
+
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+const app = require('../app.js');
+
+chai.use(chaiHttp);
+
+// Describe is like a container for our tests.
+describe('Hello World Route Test', () => {
+
+    // Test Case 1
+    it('Returns a 200 Response', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((error, response) => {
+                if (error) done(error);
+                expect(response).to.have.status(200);
+                expect(response.body).to.have.any.keys('message');
+                done();
+            });
+    });
+
+    // Test Case 2
+    it('Returns a "Hello World" message', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((error, response) => {
+                if (error) done(error);
+                expect(response.body).to.be.deep.equal({
+                    message: 'Hello, world!'
+                });
+                done();
+            });
+    });
+});
